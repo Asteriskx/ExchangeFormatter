31def5ea5bf0512f804af20f892e56e11de7836c
Asterisk <kuro5858tube@gmail.com>
2017-08-17 22:43:47 +0900

    #2 First Commit
    ExchangeFormatter Core Files upload.

---
+++ ExchangeFormat/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ExchangeFormat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExchangeButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.NowProcessCheck = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OldFormat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NewFormat = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Grep = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ExchangeButton
+            // 
+            this.ExchangeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExchangeButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ExchangeButton.ForeColor = System.Drawing.Color.GhostWhite;
+            this.ExchangeButton.Location = new System.Drawing.Point(405, 139);
+            this.ExchangeButton.Name = "ExchangeButton";
+            this.ExchangeButton.Size = new System.Drawing.Size(132, 52);
+            this.ExchangeButton.TabIndex = 0;
+            this.ExchangeButton.Text = "Exchange";
+            this.ExchangeButton.UseVisualStyleBackColor = true;
+            this.ExchangeButton.Click += new System.EventHandler(this.ExchangeButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.SteelBlue;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NowProcessCheck});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 203);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(549, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // NowProcessCheck
+            // 
+            this.NowProcessCheck.ForeColor = System.Drawing.SystemColors.Control;
+            this.NowProcessCheck.Name = "NowProcessCheck";
+            this.NowProcessCheck.Size = new System.Drawing.Size(55, 17);
+            this.NowProcessCheck.Text = "準備完了";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(30, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "OldFormat : ";
+            // 
+            // OldFormat
+            // 
+            this.OldFormat.Location = new System.Drawing.Point(128, 68);
+            this.OldFormat.Name = "OldFormat";
+            this.OldFormat.Size = new System.Drawing.Size(409, 19);
+            this.OldFormat.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(30, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NewFormat : ";
+            // 
+            // NewFormat
+            // 
+            this.NewFormat.Location = new System.Drawing.Point(128, 106);
+            this.NewFormat.Name = "NewFormat";
+            this.NewFormat.Size = new System.Drawing.Size(409, 19);
+            this.NewFormat.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(30, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "GrepPath : ";
+            // 
+            // Grep
+            // 
+            this.Grep.Location = new System.Drawing.Point(128, 30);
+            this.Grep.Name = "Grep";
+            this.Grep.Size = new System.Drawing.Size(409, 19);
+            this.Grep.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(549, 225);
+            this.Controls.Add(this.Grep);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NewFormat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OldFormat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.ExchangeButton);
+            this.Name = "Form1";
+            this.Text = "ExchangeFormatter";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExchangeButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel NowProcessCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox OldFormat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NewFormat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Grep;
+    }
+}
+
---
+++ ExchangeFormat/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+using static System.Console;
+
+namespace ExchangeFormat
+{
+    public partial class Form1 : Form
+    {
+        // Grep 対象ファイル名格納
+        private string[] files;
+
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     コンストラクタ </summary>
+        /// <returns>
+        ///     Nothing. </returns>
+        /// -----------------------------------------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     Exchangeボタンが押下された時実行します。 </summary>
+        /// <returns>
+        ///     Nothing. </returns>
+        /// -----------------------------------------------------------------------------------
+        private void ExchangeButton_Click(object sender, EventArgs e)
+        {
+            searchTargetFiles( );
+            return;
+        }
+
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     対象ファイルを検索します。 </summary>
+        /// <returns>
+        ///     Nothing. </returns>
+        /// -----------------------------------------------------------------------------------
+        private void searchTargetFiles()
+        {
+            try
+            {
+                var grepPath = Grep.Text;
+
+                // ファイル名検索の実行
+                files = GrepFiles(@"C:\Users\Asterisk\Desktop\test\", "aa", "*.jpg-large", false);
+
+                // 結果の表示
+                foreach (string f in files)
+                {
+                    WriteLine(f);
+                    // 拡張子を".jpg"に変更する
+                    // 2番目の引数は、"jpg"でも".jpg"でもどちらでも..
+                    string newName = Path.ChangeExtension(f, NewFormat.Text);
+
+                    // 実際にファイル名を変更する
+                    // fileNameがない場合や、newFileNameが存在する場合は例外がスローされる
+                    File.Move(f, newName);
+
+                    #region 別バージョン(却下)
+                    /*
+                        var targetStr = "";
+                        var targetPos = 0;
+                        var targetFolder = Path.GetFileName(Path.GetDirectoryName(grepPath));
+
+                        targetPos = f.LastIndexOf(@"\");
+                        targetStr = VBRight(f, targetPos - targetFolder.Length - 1);
+
+                        // デバッグ表示
+                        WriteLine(targetStr);
+
+                        // ファイル名の変更(コピーで対応)
+                         File.Copy(oldName, newName, true);
+                         File.Delete(oldName);
+                     */
+                    #endregion
+                }
+
+                MessageBox.Show("変換が完了しました。", "変換完了", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("処理対象が存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+
+        #region　Left メソッド
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の左端から指定された文字数分の文字列を返します。</summary>
+        /// <param name="target">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="len">
+        ///     取り出す文字数。</param>
+        /// <returns>
+        ///     左端から指定された文字数分の文字列。
+        ///     文字数を超えた場合は、文字列全体が返されます。</returns>
+        /// -----------------------------------------------------------------------------------
+        public static string VBLeft(string target, int len)
+        {
+            if (len <= target.Length)
+                return target.Substring(0, len);
+
+            return target;
+        }
+
+        #endregion
+
+        #region　Mid メソッド
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の指定された位置以降のすべての文字列を返します。</summary>
+        /// <param name="target">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="start">
+        ///     取り出しを開始する位置。</param>
+        /// <returns>
+        ///     指定された位置以降のすべての文字列。</returns>
+        /// -----------------------------------------------------------------------------------
+        public static string VBMid(string target, int start)
+        {
+            if (start <= target.Length)
+                return target.Substring(start - 1);
+
+            return string.Empty;
+        }
+
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の指定された位置から、指定された文字数分の文字列を返します。</summary>
+        /// <param name="target">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="start">
+        ///     取り出しを開始する位置。</param>
+        /// <param name="len">
+        ///     取り出す文字数。</param>
+        /// <returns>
+        ///     指定された位置から指定された文字数分の文字列。
+        ///     文字数を超えた場合は、指定された位置からすべての文字列が返されます。</returns>
+        /// -----------------------------------------------------------------------------------
+        public static string VBMid(string target, int start, int len)
+        {
+            if (start <= target.Length)
+            {
+                if (start + len - 1 <= target.Length)
+                    return target.Substring(start - 1, len);
+
+                return target.Substring(start - 1);
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
+
+        #region　Right メソッド
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の右端から指定された文字数分の文字列を返します。</summary>
+        /// <param name="target">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="len">
+        ///     取り出す文字数。</param>
+        /// <returns>
+        ///     右端から指定された文字数分の文字列。
+        ///     文字数を超えた場合は、文字列全体が返されます。</returns>
+        /// -----------------------------------------------------------------------------------
+        public static string VBRight(string target, int len)
+        {
+            if (len <= target.Length)
+                return target.Substring(target.Length - len);
+
+            return target;
+        }
+
+        #endregion
+
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     ファイルの検索を行う </summary>
+        /// <param name="dirPath">
+        ///     フォルダのパス。</param>
+        /// <param name="pattern">
+        ///     検索する正規表現のパターン。</param>
+        /// <param name="fileWildcards">
+        ///     対象とするファイル。</param>
+        /// <param name="ignoreCase">
+        ///     大文字小文字を区別するか。</param>
+        /// <returns>
+        ///     見つかったファイルパスの配列。</returns>
+        /// -----------------------------------------------------------------------------------
+        public static string[] GrepFiles(
+            string dirPath, string pattern, string fileWildcards,  bool ignoreCase )
+        {
+            ArrayList fileCol = new ArrayList();
+
+            // 正規表現のオプションを設定
+            RegexOptions opts = RegexOptions.None;
+
+            if (ignoreCase)
+                opts |= RegexOptions.IgnoreCase;
+
+            Regex reg = new Regex(pattern, opts);
+
+            // フォルダ内にあるファイルを取得
+            DirectoryInfo dir = new DirectoryInfo(dirPath);
+            FileInfo[] files  = dir.GetFiles(fileWildcards);
+
+            foreach (FileInfo f in files)
+            {
+                // 正規表現のパターンを使用して一つずつファイルを調べる
+                if (ContainTextInFile(f.FullName, reg))
+                    fileCol.Add(f.FullName);
+            }
+
+            return (string[])fileCol.ToArray(typeof(string));
+        }
+
+        /// -----------------------------------------------------------------------------------
+        /// <summary>
+        ///     ファイルの内容がpatternに一致するか調べる  </summary>
+        /// <param name="filePath">
+        ///     検索対象のファイルパス　</param>
+        /// <param name="reg">
+        ///     正規表現オブジェクト　</param>
+        /// <returns>
+        ///     一致したファイル名（フルパス）　</returns>
+        /// -----------------------------------------------------------------------------------
+        private static bool ContainTextInFile(string filePath, Regex reg)
+        {
+            //ファイルを読み込む
+            using (var strm = new StreamReader(filePath, System.Text.Encoding.Default, true))
+            {
+                string txt = "";
+                try
+                {
+                    txt = strm.ReadToEnd();
+                }
+                catch(Exception e)
+                {
+                    WriteLine(e.StackTrace);
+                }
+                finally
+                {
+                    strm.Close();
+                }
+                return reg.IsMatch(txt);
+            }
+        }
+    }
+}
